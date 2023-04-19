dc786ed2e550d30a5006edc7eda4b8d897597d4f
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-19 18:59:49 +0900

    create Footer import and Footer element inm Main.js

--- app/Main.js
+++ app/Main.js
@@ -3,34 +3,13 @@ import ReactDOM from "react-dom/client";
 //Components imports:
 import Header from "./components/Header.js";
 import HomeGuest from "./components/HomeGuest.js";
-
+import Footer from "./components/Footer.js";
 function ExampleComponent() {
   return (
     <>
       <Header />
       <HomeGuest />
-      <footer className="border-top text-center small text-muted py-3">
-        <p>
-          <a href="/" className="mx-1">
-            Home
-          </a>{" "}
-          |{" "}
-          <a className="mx-1" href="/about-us">
-            About Us
-          </a>{" "}
-          |{" "}
-          <a className="mx-1" href="/terms">
-            Terms
-          </a>
-        </p>
-        <p className="m-0">
-          Copyright &copy; 2020{" "}
-          <a href="/" className="text-muted">
-            ComplexApp
-          </a>
-          . All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 }
