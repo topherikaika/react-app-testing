93c294b488c1aa2ff4de4d9d1dc12d36a4f841bf
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-19 16:16:51 +0900

    create function ExampleComponent for testing

--- app/Main.js
+++ app/Main.js
@@ -1,2 +1,11 @@
 import React from "react"
 import React-DOM from "react-dom/client"
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This App</h1>
+      <p>Created today</p>
+    </div>
+  )
+}
