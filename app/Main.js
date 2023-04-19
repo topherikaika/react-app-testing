39372f895f1ffb08bb33f6ad34bce019fd6dc6b1
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-19 18:41:46 +0900

    Create Header in Main.js

--- app/Main.js
+++ app/Main.js
@@ -1,32 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
 
 function ExampleComponent() {
   return (
     <>
-      <header className="header-bar bg-primary mb-3">
-        <div className="container d-flex flex-column flex-md-row align-items-center p-3">
-          <h4 className="my-0 mr-md-auto font-weight-normal">
-            <a href="/" className="text-white">
-              {" "}
-              ComplexApp{" "}
-            </a>
-          </h4>
-          <form className="mb-0 pt-2 pt-md-0">
-            <div className="row align-items-center">
-              <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autocomplete="off" />
-              </div>
-              <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-                <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
-              </div>
-              <div className="col-md-auto">
-                <button className="btn btn-success btn-sm">Sign In</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </header>
+      <Header />
 
       <div className="container py-md-5">
         <div className="row align-items-center">
@@ -61,7 +40,6 @@ function ExampleComponent() {
           </div>
         </div>
       </div>
-
       <footer className="border-top text-center small text-muted py-3">
         <p>
           <a href="/" className="mx-1">
