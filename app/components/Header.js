78d9092243ae21c38f5da36952f57b7f561b9c65
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-19 18:45:35 +0900

    create return in header function in Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Header() {
+  return (
   <header className="header-bar bg-primary mb-3">
     <div className="container d-flex flex-column flex-md-row align-items-center p-3">
       <h4 className="my-0 mr-md-auto font-weight-normal">
@@ -24,6 +25,7 @@ function Header() {
       </form>
     </div>
   </header>;
+  )
 }
 
 export default Header;
