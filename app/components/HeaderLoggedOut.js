7c0ca56a801e190ca4e780c47efa0d410e7f3a12
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-26 17:13:47 +0900

    create async handleSubmit function with try await axios post

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 function HeaderLoggedOut() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+
+  async function handleSubmit() {
+    try {
+      const response = await Axios.post("http://localhost:8080/login", { username, passowrd });
+      console.log(response.data);
+    } catch (e) {
+      console.log("There was a problem");
+    }
+    e.preventDefault();
+  }
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
           <input onChange={e => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
