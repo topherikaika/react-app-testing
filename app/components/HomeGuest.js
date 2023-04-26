4e9b26653171e3c0070049464934dc15cd0dea92
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-26 16:25:30 +0900

    create async function for new user sign-up, import axios and testing db

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "./Page";
+import Axios from "axios";
 
 function HomeGuest() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await Axios.post("http://localhost:8080/register", { username: "test2", email: "test2@test.com", password: "qwerty123456" });
+      console.log("User created successfully!");
+    } catch (e) {
+      console.log("There was an error");
+    }
+  }
   return (
     <Page title="Welcome!" wide={true}>
       <div className="row align-items-center">
@@ -10,7 +20,7 @@ function HomeGuest() {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
