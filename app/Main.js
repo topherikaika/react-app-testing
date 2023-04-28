f0cb1e98c950dc5032267dabb94f2def7f0b9a7d
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-28 17:46:57 +0900

    import FlashMessages in Main.js

--- app/Main.js
+++ app/Main.js
@@ -13,6 +13,8 @@ import Terms from "./components/Terms.js";
 import Home from "./components/Home.js";
 import CreatePost from "./components/CreatePost.js";
 import ViewSinglePost from "./components/ViewSinglePost.js";
+import FlashMessages from ".components/FlashMessages.js";
+
 function Main() {
   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexAppToken")));
   return (
