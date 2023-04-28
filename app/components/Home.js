3f07afb5ad08357c25e06dd8d03ddb272804e599
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-28 11:00:14 +0900

    create interpolation to pull username from local storage

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <>
       <Page title="Your Feed">
         <h2 class="text-center">
-          Hello <strong>chris</strong>, your feed is empty.
+          Hello <strong>{localStorage.getItem("complexAppUsername")}</strong>, your feed is empty.
         </h2>
         <p class="lead text-muted text-center">Your feed displays the latest posts from the people you follow. If you don&rsquo;t have any friends to follow that&rsquo;s okay; you can use the &ldquo;Search&rdquo; feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
       </Page>
