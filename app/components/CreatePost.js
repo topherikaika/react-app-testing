031c625b4bb1a74c895c14d19d48bc9a7a07b01c
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-29 12:04:24 +0900

    import React useContext and ExampleContext, create const addFlashMessage

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Page from "./Page";
 import Axios from "axios";
+import ExampleContext from "../ExampleContext";
 
 function CreatePost(props) {
   const [title, setTiitle] = useState();
   const [body, setBody] = useState();
   const navigate = useNavigate();
+  const addFlashMessage = useContext(ExampleContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
