9208f98840f512ad632044400dea88497c58602f
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-29 15:45:21 +0900

    change src for avatar image to pull from state

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -20,7 +20,7 @@ function HeaderLoggedIn(props) {
         <span className="chat-count-badge text-white"> </span>
       </span>
       <a href="#" className="mr-2">
-        <img className="small-header-avatar" src={localStorage.getItem("complexAppAvatar")} />
+        <img className="small-header-avatar" src={appState.user.avatar} />
       </a>
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
