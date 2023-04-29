6e73ad053ab2b0208b2fa2a8a3524e69acae5e23
Christopher Ikaika Crespo <topherikaika@gmail.com>
2023-04-29 12:01:30 +0900

    create ExampleContext tag and add prop value for addFlashMessage, remove addFlashMessage prop from CreatePost route

--- app/Main.js
+++ app/Main.js
@@ -24,18 +24,20 @@ function Main() {
     setFlashMessages(prev => prev.concat(msg));
   }
   return (
-    <BrowserRouter>
-      <FlashMessages messages={flashMessages} />
-      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-      <Routes>
-        <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
-        <Route path="/post/:id" element={<ViewSinglePost />} />
-        <Route path="/create-post" element={<CreatePost addFlashMessage={addFlashMessage} />} />
-        <Route path="/about-us" element={<About />} />
-        <Route path="/terms" element={<Terms />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <ExampleContext.provider value={addFlashMessage}>
+      <BrowserRouter>
+        <FlashMessages messages={flashMessages} />
+        <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        <Routes>
+          <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+          <Route path="/post/:id" element={<ViewSinglePost />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ExampleContext.provider>
   );
 }
 
